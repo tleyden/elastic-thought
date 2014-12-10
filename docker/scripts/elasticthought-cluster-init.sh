b1d307a658b6ff5089d04e04dc5e8e6e6d056fd1
Traun Leyden <tleyden@couchbase.com>
2014-12-10 09:07:18 -0800

    use fleet to kick off

--- docker/scripts/elasticthought-cluster-init.sh
+++ docker/scripts/elasticthought-cluster-init.sh
@@ -65,16 +65,20 @@ untilsuccessful sudo docker run tleyden5iwx/couchbase-server-3.0.1 /opt/couchbas
 echo "Done: created a cbfs bucket"
 
 # kick off 3 cbfs nodes (TODO: num nodes should be a parameter)
+echo "Kick off cbfs nodes"
 git clone https://github.com/tleyden/elastic-thought.git
 cd elastic-thought/docker/fleet && fleetctl submit cbfs_node@.service && fleetctl submit cbfs_announce@.service && cd ~
 for i in `seq 1 $numnodes`; do fleetctl start cbfs_node@$i.service; fleetctl start cbfs_announce@$i.service; done
 
 # wait for all cbfs nodes to come up 
+echo "Wait for cbfs nodes to come up"
 for i in `seq 1 $numnodes`; do 
     untilsuccessful etcdctl get /services/cbfs/cbfs_node@$i
 done
+echo "Done: cbfs nodes up"
 
 # create elastic-thought bucket
+echo "Create elastic-thought bucket"
 untilsuccessful sudo docker run tleyden5iwx/couchbase-server-3.0.1 /opt/couchbase/bin/couchbase-cli bucket-create -c $COUCHBASE_CLUSTER -u $CB_USERNAME -p $CB_PASSWORD --bucket=elastic-thought --bucket-ramsize=1024
 
 # run sed on sync gateway config template
@@ -82,28 +86,29 @@ COUCHBASE_IP_PORT=$COUCHBASE_CLUSTER:8091
 sed -e "s/COUCHBASE_IP_PORT/${COUCHBASE_IP_PORT}/" elastic-thought/docker/templates/sync_gateway/sync_gw_config.json > /tmp/sync_gw_config.json
 
 # upload sync gateway config to cbfs
+echo "Upload sync gateway config to cbfs"
 ip=$(hostname -i | tr -d ' ')
 sudo docker run --net=host -v /tmp:/tmp tleyden5iwx/cbfs cbfsclient http://$ip:8484/ upload /tmp/sync_gw_config.json /sync_gw_config.json 
 
 # kick off sync gateway 
+echo "Kick off sync gateway"
 mkdir sync-gateway && \
   cd sync-gateway && \
   wget https://raw.githubusercontent.com/tleyden/sync-gateway-coreos/master/scripts/cluster-init.sh && \
   chmod +x cluster-init.sh && \
-  ./cluster-init.sh -n $numnodes -c "master" -g http://$ip:8484/sync_gw_config.json
+  ./cluster-init.sh -n $numnodes -c "master" -g http://$ip:8484/sync_gw_config.json && \
+  cd ~ 
 
 # wait for all sync gw nodes to come up 
+echo "Wait for sync gateway nodes to come up"
 for i in `seq 1 $numnodes`; do 
     untilsuccessful etcdctl get /services/sync_gw/sync_gw_node@$i
 done
+echo "Done: sync gateway nodes up"
 
+# kick off elastic-thought httpd daemons
+echo "Kick off elastic thought httpd daemons"
+cd elastic-thought/docker/fleet && fleetctl submit elastic_thought_gpu@.service && cd ~
+for i in `seq 1 $numnodes`; do fleetctl start elastic_thought_gpu@$i.service; done
 
-# Todo: use coreos init for this
-# kick off elasticthought httpd-worker (goroutine)
-echo "Starting elastic thought httpd (blocking call)"
-sudo docker run --net=host tleyden5iwx/elastic-thought httpd 
-
-# TODO: 
-# kick off nsqlookupd + sidekick
-# kick off nsq (3 nodes)
-# kick off elasticthought worker (caffe worker)
+echo "Done!  Your ElasticThought REST API is available to use on <public-ip-any-node>:8080"
