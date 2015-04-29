310f3c62ea8704e1e7424238176280744c700883
Traun Leyden <tleyden@couchbase.com>
2015-04-28 21:26:35 -0700

    implement FileSystemBlobStore

--- filesystem_blobstore.go
+++ filesystem_blobstore.go
@@ -3,6 +3,8 @@ package elasticthought
 import (
 	"io"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type FileSystemBlobStore struct {
@@ -10,6 +12,14 @@ type FileSystemBlobStore struct {
 	// the root path where blobs will be stored.  all paths will
 	// be calculated relative to this path.
 	rootPath string
+}
+
+type FileSystemBlobHandle struct{}
+
+func (h FileSystemBlobHandle) Nodes() map[string]time.Time {
+	return map[string]time.Time{
+		"filesystem": time.Now(),
+	}
 }
 
 func NewFileSystemBlobStore(rootPath string) (*FileSystemBlobStore, error) {
@@ -28,18 +38,46 @@ func NewFileSystemBlobStore(rootPath string) (*FileSystemBlobStore, error) {
 
 }
 
-func (f *FileSystemBlobStore) Get(path string) (io.ReadCloser, error) {
-	return nil, nil
+func (f FileSystemBlobStore) Get(path string) (io.ReadCloser, error) {
+
+	// build full path to file
+	path = f.absolutePath(path)
+
+	// open the file for reading and return
+	return os.Open(path)
+
 }
 
-func (f *FileSystemBlobStore) Put(srcname, dest string, r io.Reader, opts BlobPutOptions) error {
-	return nil
+func (f FileSystemBlobStore) Put(srcname, dest string, r io.Reader, opts BlobPutOptions) error {
+
+	// build full path to file
+	path := f.absolutePath(dest)
+
+	// open the file for writing
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	// copy from reader -> file
+	_, err = io.Copy(file, r)
+
+	return err
 }
 
-func (f *FileSystemBlobStore) Rm(fn string) error {
-	return nil
+func (f FileSystemBlobStore) Rm(path string) error {
+
+	// build full path to file
+	path = f.absolutePath(path)
+
+	return os.Remove(path)
+
 }
 
-func (f *FileSystemBlobStore) OpenFile(path string) (BlobHandle, error) {
-	return nil, nil
+func (f FileSystemBlobStore) OpenFile(path string) (BlobHandle, error) {
+	return FileSystemBlobHandle{}, nil
+}
+
+func (f FileSystemBlobStore) absolutePath(relativePath string) string {
+	return filepath.Join(f.rootPath, relativePath)
 }
